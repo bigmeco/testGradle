9bdba5b804b4489c2a0b8b2e19afab4ee81cee12
bigmeco <bigmecoprog@gmail.com>
2020-07-10 15:17:56 +0300

    start ci tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,23 @@
+plugins {
+    id("base")
+}
+
+
+
+
 tasks.create<Copy>("copy") {
+
     description = "Copies sources to the dest directory"
     group = "Custom"
 
     from("src")
-    into("dest")
-} 
+    into("C:\\Users\\Nikolay\\Desktop")
+
+}
+tasks.create<Zip>("zip") {
+    description = "Archives sources in a zip file"
+    group = "Archive"
+
+    from("src")
+    archiveFileName.set("basic-demo-1.0.zip")
+}
